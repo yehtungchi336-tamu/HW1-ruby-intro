1bfeaad18e4d678ecd204b4096e76a08f6103bf5
yehtungchi336-tamu <113081052+yehtungchi336-tamu@users.noreply.github.com>
2022-09-10 14:53:01 -0500

    Add files via upload

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,25 +3,63 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+    # YOUR CODE HERE
+    if arr.empty?
+        0
+    else
+        sum = 0
+        for item in arr
+            sum = sum + item
+        end
+        return sum
+    end
 end
 
 def max_2_sum(arr)
   # YOUR CODE HERE
+  if arr.empty?
+    0
+  elsif arr.length() == 1
+    arr[0]
+  else
+    arr = arr.sort
+    sum = arr[-1] + arr[-2]
+    return sum
+  end    
 end
 
-def sum_to_n?(arr, number)
+def sum_to_n? (arr, n)
   # YOUR CODE HERE
+  dict = {}
+  for i in 0..(arr.length-1)
+    if dict.key?(arr[i])
+      return true
+    end
+    dict[n-arr[i]] = i
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, " + name.to_s
 end
 
 def starts_with_consonant?(string)
   # YOUR CODE HERE
+  arr = ["A", "E", "I", "O", "U"]
+  c = string.to_s[0]
+  if string.length == 0
+    return false
+  elsif arr.include? c.upcase
+    return false
+  elsif c =~ /[[:alpha:]]/
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4?(string)
